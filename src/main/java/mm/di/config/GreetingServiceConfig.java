48b761a48d55e5bca3d11c70ecf26652295fadb2
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-24 23:14:39 +0000

    Primary Beans and Profiles
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- src/main/java/mm/di/config/GreetingServiceConfig.java
+++ src/main/java/mm/di/config/GreetingServiceConfig.java
@@ -1,9 +1,10 @@
 package mm.di.config;
 
-import mm.di.services.GoodbyeWorldGreetingService;
-import mm.di.services.HelloWorldGreetingService;
+import mm.di.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -15,5 +16,23 @@ public class GreetingServiceConfig {
     @Bean
     GoodbyeWorldGreetingService goodbyeWorldGreetingService() {
         return new GoodbyeWorldGreetingService();
+    };
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    };
+
+    @Bean("i18nService")
+    @Profile("EN")
+    I18nEnglishGreetingService I18nEnglishGreetingService() {
+        return new I18nEnglishGreetingService();
+    };
+
+    @Bean("i18nService")
+    @Profile({"default",  "ES"})
+    I18nSpanishGreetingService I18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
     };
 }
